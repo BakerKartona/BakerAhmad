7a6c91afc69a1afe449a56d5cf5baefcc3be720b
BakerIbuypower <BakerIbuypower@IbuyBaker>
2022-02-11 11:03:35 -0500

    HW day 19

--- src/day19homework/ExersiceFive.java
+++ src/day19homework/ExersiceFive.java
@@ -7,11 +7,11 @@ public class ExersiceFive {
 		ExersiceFive obj = new ExersiceFive();
 
 		String result1 = obj.toLower("espn");
-		System.out.println(result1);
+			System.out.println(result1);
 		String result2 = obj.toLower("SOCCER");
-		System.out.println(result2);
+			System.out.println(result2);
 		String result3 = obj.toLower("STRING CLASS");
-		System.out.println(result3);
+			System.out.println(result3);
 
 	}
 
