28f1dc7fb21063b3f5900d36b84e6ba98a3908e7
BakerIbuypower <BakerIbuypower@IbuyBaker>
2022-02-11 11:01:48 -0500

    HW day 10

---
+++ src/day10homework/HwQ3.java
@@ -0,0 +1,20 @@
+package day10homework;
+
+public class HwQ3 {
+	public static void main(String[] args) {
+
+		oddEven(11);
+
+	}
+
+	public static void oddEven(int numOne) {
+
+		if (numOne % 2 == 0) {
+			System.out.println("Even number");
+		} else {
+			System.out.println("Odd number");
+		}
+
+	}
+
+}
---
+++ src/day10homework/HwQ4.java
@@ -0,0 +1,24 @@
+package day10homework;
+
+public class HwQ4 {
+	public static void main(String[] args) {
+		
+		getDoWhileLoopResult(20,50);
+		
+		
+	}
+	
+	
+	
+	public static void getDoWhileLoopResult (int minOne, int maxTwo) {
+		
+		do {
+			System.out.println(minOne);
+			
+			minOne++;
+			
+		}while (minOne <= 50);
+		
+	}
+
+}
